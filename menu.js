56b7499c76402c6e618163ca5ded48b3db5ca960
maxtroner <maxtroner@gmail.com>
2025-02-07 19:21:15 -0300

    Primer commit

---
+++ menu.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("menu-container").innerHTML = `
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="index.html">Inicio</a></li>
+                    <li><a href="informacion.html">Servidor</a></li>
+                    <li><a href="noticias.html">Noticias</a></li>
+                    <li><a href="descargas.html">Descargas</a></li>
+                    <li><a href="cuentas.html">Cuentas</a></li>
+                    <li><a href="donacion.html">Donación</a></li>
+                </ul>
+            </nav>
+        </header>
+    `;
+});
